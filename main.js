baec18999a2b6e6e3bbfee6aca6adf6097744a8a
Wyatt Carss <carss.w@gmail.com>
2012-08-20 23:22:15 -0400

    Add motion with cruddy momentum.

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 event_queue = new Array();
+xvelocity = [0, 0];
+yvelocity = [0, 0];
 
 $(function() {
-	canvas = $("#game");
-    setup_keydown(canvas);
-	context = setup_game(canvas[0]);
-	setInterval(check_queue, 50);
+    div = $("#game");
+    setup_keydown(div);
+    rect = setup_game();
+    setInterval(main_loop(rect), 50);
 });
 
 function setup_keydown(canvas) {
@@ -31,10 +33,67 @@ function setup_keydown(canvas) {
     });
 }
 
-function check_queue() {
-    if (event_queue.length > 0) {
-        event = event_queue.pop();
-        console.log(event);
+function main_loop(rect) {
+    return function() {
+        if (event_queue.length > 0) {
+            event = event_queue.pop();
+            console.log(event);
+        }
+        else
+        {
+            event = null;
+            yvelocity[0] /= 5;
+            if (yvelocity[0] > -1 || yvelocity[0] < 1)
+                yvelocity[0] = 0;
+            yvelocity[1] /= 5;
+            if (yvelocity[1] > -1 || yvelocity[1] < 1)
+                yvelocity[1] = 0;
+            xvelocity[0] /= 5;
+            if (xvelocity[0] > -1 || yvelocity[0] < 1)
+                xvelocity[0] = 0;
+            xvelocity[1] /= 5;
+            if (xvelocity[1] > -1 || yvelocity[1] < 1)
+                xvelocity[1] = 0;
+        }
+
+    //console.log(rect);
+    //y = rect.y;
+    //x = rect.x;
+    //console.log(rect.getX());
+    //console.log(rect.getY());
+
+        if (event == 'up') {
+            //console.log('up');
+            if (yvelocity[0] < 20)
+                yvelocity[0] += 2;
+            rect.setY(rect.getY()-yvelocity[0]);
+            event == null;
+        }
+        if (event == 'down') {
+            //console.log('down');
+            if (yvelocity[1] < 20)
+                yvelocity[1] += 2;
+            rect.setY(rect.getY()+yvelocity[1]);
+            event == null;
+        }
+        if (event == 'left') {
+            //console.log('down');
+            if (xvelocity[0] < 20)
+                xvelocity[0] += 2;
+            rect.setX(rect.getX()-xvelocity[0]);
+            event == null;
+        }
+        if (event == 'right') {
+            //console.log('down');
+            if (xvelocity[1] < 20)
+                xvelocity[1] += 2;
+            rect.setX(rect.getX()+xvelocity[1]);
+            event == null;
+        }
+        if (event == 'restart') {
+            rect.setX(200);
+            rect.setY(200);
+        }
     }
 }
 
@@ -46,8 +105,46 @@ function key_array(obj) {
   return arr.sort();
 }
 
-function setup_game(canvas) {
-  context = canvas.getContext("2d");
-  context.fillStyle = "rgb(80, 80, 240)";
-  context.fillRect(0, 0, 500, 500);
+function setup_game() {
+    var stage = new Kinetic.Stage({
+        container: "game",
+        width: 500,
+        height: 500
+    });
+
+    var layer = new Kinetic.Layer();
+
+    var bg = new Kinetic.Rect({
+        width: 500,
+        height: 500,
+        fill: "#6666BB",
+        stroke: "black",
+        strokeWidth: 1
+    });
+
+    var rect = new Kinetic.Rect({
+        x: 239,
+        y: 75,
+        width: 100,
+        height: 50,
+        fill: "#DDDDDD",
+        stroke: "black",
+        strokeWidth: 1
+    });
+
+    console.log('x is');
+    console.log(rect.getX());
+    // add the shape to the layer
+    layer.add(bg);
+    layer.add(rect);
+
+    // add the layer to the stage
+    stage.add(layer);
+
+    stage.onFrame(function() {
+        layer.draw();
+    });
+
+    stage.start();
+    return rect;
 }
